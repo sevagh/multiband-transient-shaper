d6c0e72f68359546b69189af750e91d717426451
Sevag Hanssian <sevagh@protonmail.com>
2020-12-06 18:21:25 -0500

    More reading about envelope followers

--- banded_transient_shaper.m
+++ banded_transient_shaper.m
@@ -2,16 +2,18 @@ b = hz2bark([20, 20000]);
 barkVect = linspace(b(1), b(2), 40);
 hzVect = bark2hz(barkVect);
 
-display(hzVect)
-
 [x, fs] = audioread('simple_mix.wav');
 
 plot(x); grid on; hold on;
+
+y_reconstruct = zeros(size(x));
 
 for bands = 1:1:size(hzVect, 2)-1
    band_edges = hzVect(bands:bands+1);
    display(band_edges)
    y = bandpass(x, band_edges, fs);
    plot(y);
-   pause();
+   y_reconstruct = y_reconstruct + y;
 end
+
+audiowrite('reconstructed.wav', y_reconstruct, fs);
